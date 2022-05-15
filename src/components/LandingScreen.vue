eba464f4cba8080fa9deb55769b846ca5ca56dc9
AlfClark <alfredoclarkcardenas@gmail.com>
2022-05-14 18:15:04 -0700

    Landing section

--- src/components/LandingScreen.vue
+++ src/components/LandingScreen.vue
@@ -5,10 +5,9 @@
         <h2 class="slogan">Nosotros y el mar</h2>
         <div class="brand-name">
           <h1 class="brand">OCEANUM</h1>
-          <h2 class="dot">.</h2>
+          <h1 class="dot">.</h1>
         </div>
-
-        <h2>Desarrollamos éste proyecto con una sola finalidad:</h2>
+        <h2 class="sub">Desarrollamos éste proyecto con una sola finalidad:</h2>
         <h3>Preservar la vida en nuestros mares.</h3>
       </div>
       <div class="hero-right"></div>
@@ -26,8 +25,6 @@ export default {};
   height: 90vh;
   display: flex;
   flex-direction: column;
-  align-content: center;
-  justify-content: center;
   position: static;
 }
 .hero {
@@ -37,9 +34,29 @@ export default {};
   color: var(--carbon);
   display: flex;
 }
+.hero-left {
+  padding-left: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: left;
+  align-items: flex-start;
+}
 .slogan {
   font-size: 1rem;
 }
-.brand.name {
+.brand-name {
+  display: flex;
+}
+.brand-name h1 {
+  font-size: 3rem;
+}
+.brand {
+  letter-spacing: 5px;
+}
+.dot {
+  color: var(--water);
+}
+.sub {
+  font-size: 1rem;
 }
 </style>

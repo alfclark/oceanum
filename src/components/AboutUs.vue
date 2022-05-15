32b1f1566d1a9fd947c657ab34a1de5f5a5c754a
AlfClark <alfredoclarkcardenas@gmail.com>
2022-05-14 17:57:42 -0700

    About section added

---
+++ src/components/AboutUs.vue
@@ -0,0 +1,11 @@
+<template>
+  <div style="margin-top: -50px; position: absolute" id="about"></div>
+  <section class="about"></section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>

27f3be88c963c5fa8f0d97704f57310ef49d4ae6
AlfClark <alfredoclarkcardenas@gmail.com>
2022-05-15 22:26:50 -0700

    About us component created

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,6 +1,16 @@
 <template>
   <div style="margin-top: -50px; position: absolute" id="about"></div>
-  <section class="about"></section>
+  <section class="container">
+    <div class="about-us">
+      <h1 class="subtitle">
+        Nosotros:
+        <hr class="divider" />
+      </h1>
+      <p class="desc">
+        Conoce en qué zonas se encuentran algunas de las especies en peligro.
+      </p>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -8,4 +18,26 @@ export default {};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+.container {
+  width: 90vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.subtitle {
+  color: var(--carbon);
+}
+.desc {
+  color: var(--carbon);
+}
+.about-us {
+  width: 70vw;
+  text-align: center;
+}
+@media screen and (max-width: 550px) {
+  .about-us {
+    width: 90vw;
+  }
+}
+</style>

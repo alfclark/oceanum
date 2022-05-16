2afa553921dc0808148f1eb9ca3ef4e683f07636
AlfClark <alfredoclarkcardenas@gmail.com>
2022-05-15 22:28:57 -0700

    Projects cards created and info added

--- src/components/ProjectsView.vue
+++ src/components/ProjectsView.vue
@@ -1,18 +1,97 @@
 <template>
   <!-- Skills Section -->
-  <div style="margin-top: -50px; position: absolute" id="skills"></div>
+  <div style="margin-top: -50px; position: absolute" id="projects"></div>
   <section class="container">
     <div class="proyectos">
       <h1 class="subtitle">
         Proyectos:
         <hr class="divider" />
       </h1>
+      <p class="desc">
+        Sé parte del cambio, presentamos los proyectos vigentes para la
+        protección y cuidado de nuestras costas y océanos.
+      </p>
       <div class="proyectos-wrapper">
-        <a href="" class="proyecto">
-          <img src="" alt="" class="logo" />
-          <h2 class="title"></h2>
-          <p class="desc"></p>
-        </a>
+        <div class="proyecto">
+          <div class="org-logo">
+            <img
+              src="https://assets01.teamassets.net/assets/images/teamseas-tm-logo.png"
+              alt=""
+              class="logo"
+            />
+          </div>
+          <div class="prog-title">
+            <h1 class="programa">#TeamSeas</h1>
+          </div>
+          <div class="prog-desc">
+            <p class="desc-text">
+              Limpieza del Océano. 1 Dólar donado = 1 libra de basura que se
+              limpia.
+            </p>
+          </div>
+          <a class="donar" href="https://teamseas.org/">Donar</a>
+        </div>
+        <div class="proyecto">
+          <div class="org-logo">
+            <img
+              src="https://earthleagueinternational.org/wp-content/uploads/2020/04/earth-league-international.png"
+              alt=""
+              class="logo"
+            />
+          </div>
+          <div class="prog-title">
+            <h1 class="programa">Operación Fake Gold</h1>
+          </div>
+          <div class="prog-desc">
+            <p class="desc-text">
+              Detener la cadena de suministro ilegal de Totoaba de México a
+              China
+            </p>
+          </div>
+          <a class="donar" href="https://earthleagueinternational.org/donate/"
+            >Donar</a
+          >
+        </div>
+        <div class="proyecto">
+          <div class="org-logo">
+            <img
+              src="http://www.seiche.com/wp-content/uploads/2018/06/Ocean-CleanUp-portrait-blue.png"
+              alt=""
+              class="logo"
+            />
+          </div>
+          <div class="prog-title">
+            <h1 class="programa">The Ocean Clean Up</h1>
+          </div>
+          <div class="prog-desc">
+            <p class="desc-text">
+              Limpieza del Océano mediante el diseño de tecnología y actividades
+              de limpieza.
+            </p>
+          </div>
+          <a class="donar" href="https://theoceancleanup.com/donate/">Donar</a>
+        </div>
+        <div class="proyecto">
+          <div class="org-logo">
+            <img
+              src="https://mx.oceana.org/wp-content/uploads/sites/17/logo_es-e1631518823246.png"
+              alt=""
+              class="logo"
+            />
+          </div>
+          <div class="prog-title">
+            <h1 class="programa">Proyecto Alacranes</h1>
+          </div>
+          <div class="prog-desc">
+            <p class="desc-text">
+              Protegiendo los hábitats marinos de las costas de México contra
+              pesca furtiva y contaminación.
+            </p>
+          </div>
+          <a class="donar" href="https://oceana.ca/en/donate-oceana-canada/"
+            >Donar</a
+          >
+        </div>
       </div>
     </div>
   </section>
@@ -31,6 +110,9 @@
 .subtitle {
   color: var(--carbon);
 }
+.desc {
+  color: var(--carbon);
+}
 .proyectos {
   width: 70vw;
   margin: 4rem 0;
@@ -41,15 +123,87 @@
   flex-wrap: wrap;
   align-items: center;
   justify-content: center;
+  gap: 2rem;
+}
+.proyecto {
+  display: flex;
+  flex-direction: column;
+  background-color: var(--water);
+  align-items: center;
+  padding: 1.5rem;
+  width: 12rem;
+  height: 25rem;
+  border-radius: 1rem;
+}
+.org-logo {
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 30%;
+  background-color: white;
+  padding: 10px;
+}
+.logo {
+  width: 9rem;
+}
+.prog-title {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 20%;
+  padding-top: 1rem;
+}
+.programa {
+  font-size: 1.2rem;
+}
+.prog-desc {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 40%;
+}
+.desc-text {
+  font-size: 0.9rem;
+}
+.donar {
+  background-color: var(--orange);
+  position: relative;
+  text-decoration: none;
+  color: white;
+  font-size: 1.2rem;
+  padding: 0.3rem 1.2rem;
+  border-radius: 1.5rem;
+  bottom: 0;
+}
+.donar:hover {
+  background-color: var(--sunshine);
+  transition: 0.4s;
 }
 
-@media screen and (max-width: 900px) {
-  .skills-wrapper {
+@media screen and (max-width: 550px) {
+  .proyectos {
+    width: 90vw;
+  }
+  .proyectos-wrapper {
+    gap: 1rem;
+  }
+  .proyecto {
     display: flex;
-    flex-wrap: wrap;
+    flex-direction: column;
+    background-color: var(--water);
     align-items: center;
     justify-content: center;
-    padding: 0;
+    gap: 1rem;
+    padding: 1rem;
+    width: 9rem;
+    border-radius: 1rem;
+  }
+  .logo {
+    width: 6rem;
+  }
+  .programa {
+    font-size: 1rem;
   }
 }
 </style>

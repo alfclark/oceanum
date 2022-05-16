aec0a92da355d7366bb567ae5d4a0cd10de913ea
AlfClark <alfredoclarkcardenas@gmail.com>
2022-05-15 22:28:26 -0700

    Map component created

---
+++ src/components/MapView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div style="margin-top: -100px; position: absolute" id="map"></div>
+  <section class="container">
+    <div class="map">
+      <h1 class="subtitle">
+        Mapa Interactivo:
+        <hr class="divider" />
+      </h1>
+      <p class="desc">
+        Conoce en qué zonas se encuentran algunas de las especies en peligro.
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+  width: 90vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.subtitle {
+  color: var(--carbon);
+}
+.desc {
+  color: var(--carbon);
+}
+.map {
+  width: 70vw;
+  text-align: center;
+}
+@media screen and (max-width: 550px) {
+  .map {
+    width: 90vw;
+  }
+}
+</style>

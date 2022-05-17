b80126ff9f3a5a91ce2b0d91ca2f47f59f951941
AlfClark <alfredoclarkcardenas@gmail.com>
2022-05-16 21:41:09 -0700

    About description paragraph

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="margin-top: -50px; position: absolute" id="about"></div>
+  <div style="margin-top: -100px; position: absolute" id="about"></div>
   <section class="container">
     <div class="about-us">
       <h1 class="subtitle">
@@ -7,7 +7,18 @@
         <hr class="divider" />
       </h1>
       <p class="desc">
-        Conoce en qué zonas se encuentran algunas de las especies en peligro.
+        Oceanum significa "Océano" en Latín. Un día, algo que inició como una
+        simple idea para competir en un hackathon, nos ayudó a darnos cuenta de
+        la gran desinformación que existe en la gente, en lo que respecta a la
+        contaminación del océano y las especies que están en peligro de
+        extinción que habitan en ellos. Por lo cual, sumamos esfuerzos y
+        conocimientos para desarrollar una herramienta, que ayude a las personas
+        a conocer más acerca de los mares y océanos Mexicanos, las especies que
+        viven ellos, y los problemas que enfrentan esos ecosistemas. A su vez,
+        presentamos ante la gente, a distintas asociaciones que trabajan
+        arduamente en la preservación de los océanos y sus especies, para que
+        también el público en general pueda sumarse al apoyo de estas grandes
+        causas. Ya sea con voluntariado o de forma económica.
       </p>
     </div>
   </section>
@@ -22,6 +33,7 @@ export default {};
 .container {
   width: 90vw;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 }
